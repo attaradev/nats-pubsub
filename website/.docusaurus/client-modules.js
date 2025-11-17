733b04b7e46d07a29fd0cfb1c800d54bb5ee04c9
Mike Attara <mpyebattara@gmail.com>
2025-11-17 22:28:34 +0000

    refactor: improve organization

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/mikeattara/Projects/Lab/nats-pubsub/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/mikeattara/Projects/Lab/nats-pubsub/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.5_esbuild@0.27.0_react-dom@18.3.1_rea_f39ef3ebaecbbc730c86faa8a3442d89/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/mikeattara/Projects/Lab/nats-pubsub/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.5_esbuild@0.27.0_react-dom@18.3.1_rea_f39ef3ebaecbbc730c86faa8a3442d89/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/mikeattara/Projects/Lab/nats-pubsub/website/src/css/custom.css"),
-];
